6366914015b4d6c96ba49750b5580f3aaca83187
xuzhenglun <xuzhenglun@gmail.com>
2015-03-07 16:32:08 +0800

    03-树3. Tree Traversals Again (25)

---
+++ 3-3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define null -1
+
+typedef struct Node
+{
+    int date;
+} node;
+
+void treeprinter(node *p,int i)
+{
+    if(p[2*i].date!=0) treeprinter(p,2*i);
+    if(p[2*i+1].date!=0) treeprinter(p,2*i+1);
+    printf("%d",p[i].date);
+    if(i!=1) printf(" ");
+}
+
+int main(void)
+{
+    int MAX,n=1;
+    node *p=(node *)malloc((MAX<<2)*sizeof(node));
+    scanf("%d",&MAX);
+
+
+
+    for(int i=1; i<=MAX<<1; i++)
+    {
+
+        char input[5]= {0};
+        int  num=null;
+        scanf("%4s",input);
+        if(!strcmp(input,"Push")) scanf("%d",&num);
+
+        if(!strcmp(input,"Push"))
+        {
+
+            p[n].date=num;
+            n=n<<1;
+        }
+
+        if(!strcmp(input,"Pop"))
+        {
+            if(n%2==0) n++;
+            else
+            {
+                n=n/2+1;
+                while(!p[n/2].date) n=n/2;
+            }
+
+        }
+    }
+    treeprinter(p,1);
+    free(p);
+    return 0;
+}
